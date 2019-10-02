01e36ecf7d4e12734b0c273edd2c8839d7e85643
nus <nus@dunamu.com>
2019-10-02 14:45:31 +0900

    v0.2

--- examples/test1.js
+++ examples/test1.js
@@ -2,8 +2,11 @@ const LambdaStreamClient = require('./../index');
 
 async function test() {
     const endPoint = 'ws://127.0.0.1:10090';
-    const lChainId = '12345';
-    const authorization = 'UXp9yJGecDJyjw5em6vsfhKCoGssKSqMZWaTDfWJ34Xvwas7Qv4F98zia4i9gbnV';
+    const lChainId = '6555545119154595912';
+
+    //authorization == groupId
+    //const authorization = 'test' + 'nus';
+    const authorization = 'test' + new Date().getTime();
 
     try {
         let streamClient = new LambdaStreamClient(endPoint, lChainId, authorization);
@@ -29,47 +32,54 @@ async function test() {
         /////////////////////
 
 
-
-
         //message event
-        streamClient.on('started', async function started() {
+        streamClient.on('STARTED', async function started() {
             console.info('[out side] started');
         });
 
-        streamClient.on('paused', async function paused() {
+        streamClient.on('PAUSED', async function paused() {
             console.info('[out side] paused');
         });
 
-        streamClient.on('resumed', async function paused() {
+        streamClient.on('RESUMED', async function paused() {
             console.info('[out side] resumed');
         });
 
-        streamClient.on('closed', async function paused() {
+        streamClient.on('CLOSED', async function paused() {
             console.info('[out side] closed');
         });
 
-        streamClient.on('receipt', async function receipt(msg) {
-            console.info(`[out side] receipt: ${msg}`);
+        streamClient.on('COMMITTED', async function committed() {
+            console.info('[out side] committed');
+        });
+
+        streamClient.on('RECEIPT', async function message(messages) {
+            //console.info(`[out side] : ${messages}`);
+            console.info(`[out side] receipt, size: ${messages.length}, ${messages[0].data.offset} - ${messages[messages.length - 1].data.offset}`);
+
+
+            let lastOffset = messages[messages.length - 1].data.offset;
+
+            await streamClient.commit(lastOffset);
+
+            await streamClient.pause();
         });
 
 
         /////////////////////////////////////////////
 
         await streamClient.start();
-/*
-        setTimeout(async function pause() {
-            await streamClient.pause();
-        }, 5000);
 
-        setTimeout(async function resume() {
+
+        setInterval(async function () {
             await streamClient.resume();
-        }, 10000);
+        }, 1000 * 20);
 
-
-        setTimeout(async function close() {
-        //    await streamClient.close();
-        }, 15000);
-*/
+        /*
+                setTimeout(async function close() {
+                    //    await streamClient.close();
+                }, 15000);
+          */
 
     } catch (err) {
         console.error(err);
---
+++ test/integration/Integration.test.js
@@ -0,0 +1,95 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+const assert = require('assert');
+const expect = chai.expect;
+const delay = require('delay');
+
+const LambdaStreamClient = require('./../../index');
+
+describe('AllInOne', function () {
+        before(async function () {
+
+        });
+        after(() => {
+            sinon.restore();
+        });
+
+        describe('[valid] integration all in one', () => {
+            it('', async () => {
+                const endPoint = 'ws://127.0.0.1:10090';
+                const lChainId = '6555545119154595912';
+
+                //authorization == groupId
+                //const authorization = 'test' + 'nus';
+                const authorization = 'test' + new Date().getTime();
+
+                let streamClient = new LambdaStreamClient(endPoint, lChainId, authorization);
+
+                //native socket event
+                streamClient.on('open', async function open() {
+                    console.info('[out side] open');
+                });
+
+                streamClient.on('close', async function close() {
+                    console.info('[out side] close');
+                });
+                streamClient.on('error', async function error(err) {
+                    console.info('[out side] error', err);
+                });
+
+
+                //message event
+                streamClient.on('STARTED', async function started() {
+                    console.info('[out side] started');
+                });
+
+                streamClient.on('PAUSED', async function paused() {
+                    console.info('[out side] paused');
+                });
+
+                streamClient.on('RESUMED', async function paused() {
+                    console.info('[out side] resumed');
+                });
+
+                streamClient.on('CLOSED', async function paused() {
+                    console.info('[out side] closed');
+                });
+
+                streamClient.on('COMMITTED', async function committed() {
+                    console.info('[out side] committed');
+                });
+
+                streamClient.on('RECEIPT', async function message(messages) {
+                    //console.info(`[out side] : ${messages}`);
+                    console.info(`[out side] receipt, size: ${messages.length}, ${messages[0].data.offset} - ${messages[messages.length - 1].data.offset}`);
+
+                    let lastOffset = messages[messages.length - 1].data.offset;
+
+                    await streamClient.commit(lastOffset);
+                    await streamClient.pause();
+                });
+
+
+                /////////////////////////////////////////////
+
+                await streamClient.start();
+
+
+                setInterval(async function () {
+                    await streamClient.resume();
+                }, 1000 * 15);
+
+
+                setTimeout(async function close() {
+                    await streamClient.close();
+
+                }, 1000 * 20);
+
+
+                await delay(1000 * 60);
+                process.exit(0);
+            });
+        });
+    }
+);
